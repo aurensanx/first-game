155d87bd83022a0ce5814adb6a45b5de1a802d03
Alberto Aurensanz <aaurensanz@hiberus.com>
2020-07-23 15:33:51 +0200

    Updates

---
+++ precache-manifest.eae70e64967b90d7d34691a3d2d6b167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16fed916e878456abb0bcff2fd06f77",
+    "url": "/first-game/index.html"
+  },
+  {
+    "revision": "4cbe0b550bf72a59b876",
+    "url": "/first-game/static/css/main.e07bb3a5.chunk.css"
+  },
+  {
+    "revision": "8529572094812f83c0ea",
+    "url": "/first-game/static/js/2.28665d50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-game/static/js/2.28665d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbe0b550bf72a59b876",
+    "url": "/first-game/static/js/main.18a7d5c5.chunk.js"
+  },
+  {
+    "revision": "f70dea4e02c32bc3c0d8",
+    "url": "/first-game/static/js/runtime-main.ad8f6f6c.js"
+  }
+]);
